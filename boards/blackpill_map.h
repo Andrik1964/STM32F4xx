7086f3636b9759e398e4ef5d5dfdb7c9a05e0d70
Andrik1964 <eprst1964@gmail.com>
2026-09-12 12:31:28 +0300

    Update step and direction pin definitions for BlackPill

--- boards/blackpill_map.h
+++ boards/blackpill_map.h
@@ -53,15 +53,15 @@
 
 // Define step pulse output pins.
 #define STEP_PORT               GPIOA
-#define X_STEP_PIN              0
-#define Y_STEP_PIN              2
-#define Z_STEP_PIN              4
+#define X_STEP_PIN              7       // Ваша схема: PA7
+#define Y_STEP_PIN              1       // Ваша схема: PA1
+#define Z_STEP_PIN              2       // Ваша схема: PA2
 #define STEP_OUTMODE            GPIO_MAP
 
 #define DIRECTION_PORT          GPIOA
-#define X_DIRECTION_PIN         1
-#define Y_DIRECTION_PIN         3
-#define Z_DIRECTION_PIN         5
+#define X_DIRECTION_PIN         6       // Ваша схема: PA6
+#define Y_DIRECTION_PIN         0       // Ваша схема: PA0
+#define Z_DIRECTION_PIN         3       // Ваша схема: PA3
 #define DIRECTION_OUTMODE       GPIO_MAP
 
 // Define stepper driver enable/disable output pin.
